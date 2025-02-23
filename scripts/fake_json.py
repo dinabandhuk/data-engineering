b5dc5efbda5df6e899a2c0ec7fe745ec0fe5a7c0
Dinabandhu Khatiwada <dinabandhukhatiwada@gmail.com>
2025-02-23 19:30:06 +0545

    create fake json

---
+++ scripts/fake_json.py
@@ -0,0 +1,21 @@
+import json
+from faker import Faker
+
+output = open("./scripts/data.JSON", "w")
+fake = Faker()
+alldata = {}
+alldata['records'] = []
+
+for x in range(10000):
+    data={
+        "name": fake.name(),
+        "age": fake.random_int(min=18, max=100, step=1),
+        "street": fake.street_address(),
+        "city": fake.city(),
+        "zip": fake.zipcode(),
+        "lng": float(fake.longitude()),
+        "lat": float(fake.latitude()),
+    }
+    alldata["records"].append(data)
+
+json.dump(alldata, output)
